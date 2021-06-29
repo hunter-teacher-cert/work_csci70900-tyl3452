264aa727b9a890051781f09070eff4f5ae006a86
tyl3452 <tayou.liu@gmail.com>
2021-06-29 12:36:59 -0400

    my first nim

---
+++ 01/Nim.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Nim{
+	
+	/**
+	 * @desc check if current stones are 0
+	 * no error checking or neg number checking
+	 * 
+	 * @param int numberOfStones
+	 * @return boolean game is over if stones is 0
+	 */
+	public static boolean isGameOver(int numberOfStones) {
+		return (numberOfStones == 0);	
+	}//end isGameOver
+	
+
+	public static void main(String[] args) {
+		System.out.println("Eric Liu - Nim");
+		
+		int stones = 12;
+		
+		while(!isGameOver(stones)) {			
+			//State how many stones they currently are
+			System.out.println("There are currently " + stones + " stones.");
+			
+			//ask user amt of stones to remove
+			Scanner in = new Scanner(System.in);
+			System.out.print("Enter 1-3 stones to remove:");
+			int stonesToRemove = in.nextInt();
+			stones -= stonesToRemove;
+			
+			//print out current number of stones
+			System.out.printf("After user turn, There are %d stones.\n", stones);
+			
+			//check if you win
+			if (isGameOver(stones)) {
+				System.out.println("User is the winner!");
+				break;
+			}
+					
+			//aiTurn
+			//ai randomly selects number of stones to remove
+			Random rand = new Random();
+			int randomStones = rand.nextInt(3)+1;
+			System.out.printf("AI will remove %d stones.\n", randomStones);
+			stones -= randomStones;
+					
+			//print out curent number of stones
+			System.out.printf("After AI Turn There are %d stones.\n", stones);
+			
+			//check if ai wins
+			if (isGameOver(stones)) {
+				System.out.println("User is the winner!");
+				break;
+			}
+
+		}//end while
+		
+		
+		
+	}//end main
+}//end class
