962fbad418694d1c8ffddaa1dff5a7ce5339b878
tyl3452 <tayou.liu@gmail.com>
2021-06-30 11:50:22 -0400

    team jiyoon marina

---
+++ 02/Mancala.java
@@ -0,0 +1,26 @@
+/*
+ *Programming in a High Level Language
+ *In Class assignment - 6/27/2021
+ *Mancala
+*/
+
+import java.util.Scanner;
+
+public class Mancala{
+
+	/**
+	*
+	*/
+	public static boolean isGameOver(int stones) {
+		return (stones == 0);
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}//end main
+
+}
