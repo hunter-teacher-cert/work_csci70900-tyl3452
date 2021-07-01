a3e4d8c6495dfa272f888c5fceea9e33960659ad
tyl3452 <tayou.liu@gmail.com>
2021-07-01 13:32:37 -0400

    up to sumrow2

--- 3/TwoDimArray.java
+++ 3/TwoDimArray.java
@@ -6,11 +6,11 @@
 
 /**
  * Team
- * Michele P.
+ * Michele Persaud
  * Eric Liu
  **/
 
-public class TwoDimArray
+public class Main
 {
 
     //print each row of 2D integer array a on its own line,
@@ -36,31 +36,51 @@ public class TwoDimArray
         }
     }
 
-
     //return sum of all items in 2D integer array a
     public static int sum1( int[][] a ) {
-        return 0;
-    }
+        int total = 0;
+        for(int j = 0; j < a.length; j++) {   // j for rows; loop for outer rows
+            for(int k = 0; k < a[j].length; k++) {    // k for columns-loop for each row, to access each column
+                total = total + a[j][k];//sum for 0+5 = 5;  5+5 = 10 +5 = 15 + 0
+            }//end for k
+        }//end for j
 
+        return total;
+    }
+    // [5,5,5]
+    // [2,9,8]
+    // [7,9,2]
 
     //return sum of all items in 2D integer array a
     // using helper fxn sumRow
     public static int sum2( int [][] m ) {
-        return 0;
+        int total = 0;
+        for(int j = 0; j < m.length; j++) {   // j for rows; loop for outer rows
+            total = total + sumRow(j, m);
+        }//end for j
+        return total;
     }
 
 
     //return sum of all items on row r of 2D integer array a
     // using a FOR loop
     public static int sumRow( int r, int[][] a ) {
-        return 0;
+        for(int k = 0; k < a[r].length; k++) {    // k for columns-loop for each row, to access each column
+            total = total + a[r][k];
+        }//end for k
+
+        return total;
     }
 
 
     //return sum of all items on row r of 2D integer array a
     // using a FOREACH loop
     public static int sumRow2( int r, int[][] m ) {
-        return 0;
+        for(int item : m[r]) {
+            total = total + item;
+        }//end for k
+
+        return total;
 
     }
 
@@ -77,12 +97,12 @@ public class TwoDimArray
         print2(m1);
         print2(m2);
         print2(m3);
-                /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
-
         System.out.print("testing sum1...\n");
         System.out.println("sum m1 : " + sum1(m1));
         System.out.println("sum m2 : " + sum1(m2));
         System.out.println("sum m3 : " + sum1(m3));
+                        /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
+
         System.out.print("testing sum2...\n");
         System.out.println("sum m1 : " + sum2(m1));
         System.out.println("sum m2 : " + sum2(m2));
