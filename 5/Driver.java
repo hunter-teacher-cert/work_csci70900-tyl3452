f85ddf0f63d0436af7b7e192f1cea06abb571ee4
tyl3452 <tayou.liu@gmail.com>
2021-07-06 11:41:55 -0400

    -new files

---
+++ 5/Driver.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Team 1
+ * Peter Tsun
+ * Chaouiki H.
+ * Eric Liu tayou.liu@gmail.com
+ **/
+
+public class Driver {
+
+  public static void main(String[] args) {
+
+    //declare a var of appropriate type to assign an instance of Time to
+    Time t;
+
+    //assign var the address of a newly-apportioned Time object
+    t = new Time();
+  }//end main()
+
+}//end class
---
+++ 5/Time.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Team 1
+ * Peter Tsun
+ * Chaouiki H.
+ * Eric Liu tayou.liu@gmail.com
+ **/
+
+public class Time {
+
+// your implementation of class Time here
+
+}//end class
