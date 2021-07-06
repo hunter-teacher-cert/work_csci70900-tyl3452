36dd8baab4e13aa908b74d0457a6547156b7277e
tyl3452 <tayou.liu@gmail.com>
2021-07-06 09:46:59 -0400

    updated class name

--- 5/Fib.java
+++ 5/Fib.java
@@ -5,20 +5,26 @@ import java.io.*;
  * Team 1
  * Peter Tsun
  * Chaouiki H.
- * Eric Liu
+ * Eric Liu tayou.liu@gmail.com
  **/
 
 public class Fib {
 
-  public int fib(int n) {
+  public static int fib(int n) {
+    if(n==0 || n==1){
+      return  n;
+    }
+    else {
+      return fib(n-1) + fib(n-2);
+    }
+
+
     // given a number, argument n
     // if gotten to base case, when number is 0, return 0;
     // add the number with the function with 1 less than the current number
     // and 2 less than the current number
 
 
-
-    return fib(n-1) + fib(n-2);
 
   }//end Fib
 
@@ -29,11 +35,11 @@ public class Fib {
     //f1 = fib(1);
     //f2 = fib(2);
 
-    for (int i = 1; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
       System.out.println("Fibonnaci of " + i + ": " + fib(i) );
     }//end for
 
-    }
+
 
   }//end main
 
