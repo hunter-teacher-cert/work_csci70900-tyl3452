90d9d27922fa4796e73a86c97650d213da28f1d6
tyl3452 <tayou.liu@gmail.com>
2021-07-07 09:39:26 -0400

    am i done?

--- 5/Driver.java
+++ 5/Driver.java
@@ -17,21 +17,21 @@ public class Driver {
 
     //assign var the address of a newly-apportioned Time object
     t1 = new Time();
-    System.out.print("t1 = " + t1.toString());
+    System.out.print("t1 = " + t1);
 
     Time t2 = null;
     try {
-      System.out.print("t2 = " + t2.toString());
+      System.out.print("t2 = " + t2);
     } catch (Exception e) {
       System.out.println("Caught exception, skip");
     }
 
     Time t3 = t1;
     Time.displayTime(t3);
-    System.out.print("t3 using tostring = " + t3.toString());
+    System.out.print("t3 using tostring = " + t3);
 
     Time t4 = new Time(1,42,0.0);
-    System.out.print("t4 = " + t4.toString());
+    System.out.print("t4 = " + t4);
     System.out.print("Is t4 equals to t1?: ");
     System.out.println(t4.equals(t1));
 
@@ -42,18 +42,18 @@ public class Driver {
     System.out.print("Adding two times t4 + t5: ");
     System.out.print(t4.toString() + " + ");
     System.out.print(t5.toString() + " = ");
-    System.out.print(t4.add(t5).toString() + "\n");
+    System.out.print(t4.add(t5) + "\n");
 
     Time t6 = new Time(14,56,54.0);
-    System.out.print("t6 : " + t6.toString());
+    System.out.print("t6 : " + t6);
     Time t7 = t4.add(t6);
-    System.out.print("t7 = t4+t6 = " + t7.toString());
+    System.out.print("t7 = t4+t6 = " + t7);
 
     //test 24 hour overflow
     Time t8 = new Time(23,27,27.0);
-    System.out.print("t8: " + t8.toString());
+    System.out.print("t8: " + t8);
     Time t9 = t8.add(t7);
-    System.out.print("t9 = t8+t7: " + t9.toString());
+    System.out.print("t9 = t8+t7: " + t9);
 
 
   }//end main()
