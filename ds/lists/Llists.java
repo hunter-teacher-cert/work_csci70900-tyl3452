e05bb20217de2c9b76c9dac2c2cdf2f64bb1f77e
tyl3452 <tayou.liu@gmail.com>
2021-07-14 15:01:10 -0400

    addfront

--- ds/lists/Llists.java
+++ ds/lists/Llists.java
@@ -6,33 +6,34 @@ public class Llist{
     private Node front; // the front of the list
 
     public  Llist(){
-	front = null;
-    }
+	    front = null;
+    }//default constructor
 
     // Add a new node containing data
     // at the front of the list
     public void addFront(String data){
-	// make the new node
-	
-	// point it to what front points to
+        // make the new node
+        Node newNode = new Node(data);
+        // point it to what front points to
+        newNode.setNext = front.getNext();
+        // point front to the new node
+        front.setNext(newNode);
 
-	// point front to the new node
-	
-    }
+    }//end addFront()
 
     public String toString(){
-	Node currentNode;
-	currentNode = front;
-	String result = "";
-	while (currentNode != null){
-	    result = result + currentNode;
-	    // this is like i=i+1 is for arrays
-	    // but for linked lists 
-	    currentNode = currentNode.getNext();
-	}
-	result = result + "null";
-	return result;
+        Node currentNode;
+        currentNode = front;
+        String result = "";
+        while (currentNode != null){
+            result = result + currentNode;
+            // this is like i=i+1 is for arrays
+            // but for linked lists 
+            currentNode = currentNode.getNext();
+        }
+        result = result + "null";
+        return result;
 
-    }
+    }//end toString()
     
-}
+}//end class Llists
