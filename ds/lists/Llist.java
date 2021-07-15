d83c3efd76a24bfc76a445f5a0f9cb212b505607
tyl3452 <tayou.liu@gmail.com>
2021-07-15 10:25:53 -0400

    llist done?

--- ds/lists/Llist.java
+++ ds/lists/Llist.java
@@ -45,4 +45,120 @@ public class Llist{
 
     }//end toString()
 
+    // returns True if there is nothing in the list
+    // False otherwise
+    public boolean isEmpty(){
+      if(front == null) {
+        return true;
+      }
+      return false;
+    }
+
+    // returns the number of items in the list
+    // Hint: look at the toString
+    // to remind you how to traverse down the list
+    public int length(){
+      int counter = 0;
+      Node currentNode = front;
+      while(currentNode != null){
+        counter++;
+        currentNode = currentNode.getNext();
+      }
+      return counter;
+    }
+
+    // returns the item at location index;
+    // returns null if there aren't enough
+    // items. Starts with index 0
+    public String get(int index){
+      int i = 0;
+      Node currentNode = front;
+      while(currentNode != null){
+        if(i == index){
+          return currentNode.getData();
+        }
+        i++;
+        currentNode = currentNode.getNext();
+      }
+      return null;
+    }
+
+    // sets the item at location index (starting
+    // with 0) to value.
+    // only sets if the index is within range
+    public void set(int index, String value){
+      int i = 0;
+      Node currentNode = front;
+      while(currentNode != null){
+        if(i == index){
+          currentNode.setData(value);
+          break;
+        }
+        i++;
+        currentNode = currentNode.getNext();
+      }
+    }
+
+    // insert an item before index.
+    // only inserts if the index is within bounds
+    // Hint: look at toString for hints on
+    // traversal and draw out a diagram.
+    // You will need a variable that refers to
+    // the node BEFORE you want to do the insertion.
+
+    // 0         1     2          3
+    // apple --> b --> carrot --> null
+    // ll.insert(2,"banana");
+    // apple --> b --> banana --> carrot --> null
+
+    public void insert(int index, String value){
+      int i = 0;
+      Node currentNode = front;
+      while(currentNode != null){
+        if(i == index-1){
+          Node newNode = new Node(value,currentNode.getNext());
+          currentNode.setNext(newNode);
+          break;
+        }
+        i++;
+        currentNode = currentNode.getNext();
+      }
+    }
+
+    // returns the index of the first item with
+    // data value key. Returns -1 if not found
+    public int search(String key){
+      int i = 0;
+      Node currentNode = front;
+      while(currentNode != null){
+        if(currentNode.getData() == key){
+          return i;
+        }
+        i++;
+        currentNode = currentNode.getNext();
+      }
+      return -1;
+    }
+
+    // removes the node at index.
+    // does nothing if index out of bounds
+
+    // 0         1     2          3
+    // apple --> b --> banana --> carrot --> null
+    // ll.remove(2);
+    // 0         1     2
+    // apple --> b --> carrot --> null
+    public void remove(int index){
+      int i = 0;
+      Node currentNode = front;
+      while(currentNode != null){
+        if(i == index-1){
+          currentNode.setNext(currentNode.getNext().getNext());
+          break;
+        }
+        i++;
+        currentNode = currentNode.getNext();
+      }
+    }
+
 }//end class Llists
