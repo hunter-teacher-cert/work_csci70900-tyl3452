c71ef42ea18738f34f03004775754bc381fbe21d
tyl3452 <tayou.liu@gmail.com>
2021-07-21 09:20:52 -0400

    moved dlist to own folder

---
+++ ds/dlist/DlistDriver.java
@@ -0,0 +1,26 @@
+/**
+ * Team 1
+ * Victoria B.
+ * Brian M.
+ * Eric Liu
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class DlistDriver{
+    public static void main(String[] args) {
+        
+        Node a = new Node("a");
+        Node b = new Node("b");
+        Node c = new Node("c");
+
+        Dlist n = new Dlist();
+        n.addFront(b);
+        n.addFront(a);
+        n.addBack(c);
+        
+
+
+    }//end main()
+}//end Driver class
