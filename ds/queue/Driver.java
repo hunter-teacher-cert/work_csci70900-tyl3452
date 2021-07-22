5d5da61a496948df888e9a6cc0b0df2987c9f309
tyl3452 <tayou.liu@gmail.com>
2021-07-22 08:34:10 -0400

    updated debug output

--- ds/queue/Driver.java
+++ ds/queue/Driver.java
@@ -14,28 +14,28 @@ public class Driver{
         //Test load q
         q.enqueue("a");
         System.out.println("Add a: " + q);
-        System.out.println("Test size: " + q.size());
+        System.out.println("\tTest size: " + q.size());
         q.enqueue("b");
         System.out.println("Add b: " + q);
-        System.out.println("Test front: " + q.front());
+        System.out.println("\tTest front: " + q.front());
         q.enqueue("c");
         System.out.println("Add c: " + q);  //Expected a->b->c->null
-        System.out.println("Test isEmpty: " + q.isEmpty());
-        System.out.println("Test front: " + q.front());
+        System.out.println("\tTest isEmpty: " + q.isEmpty());
+        System.out.println("\tTest front: " + q.front());
 
 
         //Test dequeue
         q.dequeue();
         System.out.println("dequeue 1: " + q);  // b->c->null
-        System.out.println("Test front: " + q.front());
+        System.out.println("\tTest front: " + q.front());
         q.dequeue();
         System.out.println("dequeue 2: " + q);  // c-> null
-        System.out.println("Test size: " + q.size());
+        System.out.println("\tTest size: " + q.size());
         q.dequeue();
         System.out.println("dequeue 3: " + q);  // null
-        System.out.println("Test isEmpty: " + q.isEmpty());
-        System.out.println("Test front: " + q.front());
-        System.out.println("Test size: " + q.size());
+        System.out.println("\tTest isEmpty: " + q.isEmpty());
+        System.out.println("\tTest front: " + q.front());
+        System.out.println("\tTest size: " + q.size());
 
 
 
